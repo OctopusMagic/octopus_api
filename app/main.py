21acff45e11bdee50471652b6c343a5cf637e8b7
Marcelo Cerritos <jose.marcelo.1706@gmail.com>
2024-08-15 12:05:20 -0600

    Añadir Política CORS para Acceder desde Dashboard

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
 from app.config import openapi_config
@@ -11,6 +12,14 @@ app = FastAPI(
     title=openapi_config.name,
     description=openapi_config.description,
     version=openapi_config.version,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://dashboard.octopus.local"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 logger.info("Initializing app")
